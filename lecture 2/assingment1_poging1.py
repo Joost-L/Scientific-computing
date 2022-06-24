0f1383300ccd43f03c833198b26077f5b8f144d8
NielsAH <71444846+NielsAH@users.noreply.github.com>
2022-06-24 16:49:19 +0200

    Comments

--- lecture 2/assingment1_poging1.py
+++ lecture 2/assingment1_poging1.py
@@ -10,7 +10,9 @@ def rad2cart(data, r, theta, x, y):
         values = []
         for ycord in y:
             
+            #omrekening naar poolcoördinaten
             rval = np.sqrt(xcord**2 + ycord**2)
+            #ro en r1 zijn de punten waar (x,y ) in poolcoördinaten tussen ligt
             r0 = int((rval - r[0]) // hr)
             r1 = r0 + 1
             
@@ -20,17 +22,22 @@ def rad2cart(data, r, theta, x, y):
             
             if r1 < len(r) and t1 < len(theta):
                 s = 0            
+                #feitelijk worden de ingesloten oppervlaktes hier berekend, als in 
+                #het verslag uitgelegd is dat hier niet helemaal de juiste methode
                 s += data[r1][t1]*((rval - r[r0])/hr)*((tval - theta[t0])/ht)
                 s += data[r1][t0]*((rval - r[r0])/hr)*((theta[t1] - tval)/ht)
                 s += data[r0][t1]*((r[r1] - rval)/hr)*((tval - theta[t0])/ht)
                 s += data[r0][t0]*((r[r1] - rval)/hr)*((theta[t1] - tval)/ht)
                 values.append(s)
             else: 
-                values.append(-1) #programma kan enkel interpoleren, niet extrapoleren
+                #programma kan enkel interpoleren, niet extrapoleren. Om in dat geval
+                #0 neer te zetten is een arbitraire keuze.
+                values.append(0) 
                 
         result.append(values)
     return result
 
+#een simpele test voor de werking met specifieke waardes
 def testrad2cart():
     data = [[0,1], [2,3]]
     r = [1,2]
